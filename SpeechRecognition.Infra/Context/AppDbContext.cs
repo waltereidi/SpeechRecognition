735b5cfe820594fda5bb0b896dcc44660c96f932
walter <walter-eidi@hotmail.com>
2026-01-09 20:22:36 -0300

    iniciado implementação de infraestrutura

---
+++ SpeechRecognition.Infra/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechRecognition.Infra.Context
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
