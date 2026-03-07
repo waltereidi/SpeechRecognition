c11653993de038e114c0b356949700ab08ca8296
walter <walter-eidi@hotmail.com>
2026-03-07 20:05:54 -0300

    iniciado implementação de infraestrutura para Google.FireStore

---
+++ SpeechRecognition.Infra.FireStore/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Google.Cloud.Firestore;
+
+namespace SpeechRecognition.Infra.FireStore.Context
+{
+    public class FirestoreDbContext
+    {
+        private readonly FirestoreDb _firestore;
+
+        public FirestoreDbContext(string projectId, string credentialPath)
+        {
+            Environment.SetEnvironmentVariable(
+                "windy-ellipse-399512S",
+                credentialPath
+            );
+
+            _firestore = FirestoreDb.Create(projectId);
+        }
+
+        public FirestoreDb Db => _firestore;
+
+        public CollectionReference Users => _firestore.Collection("users");
+
+        public CollectionReference Messages => _firestore.Collection("messages");
+
+        public CollectionReference Audios => _firestore.Collection("audios");
+    }
+}
